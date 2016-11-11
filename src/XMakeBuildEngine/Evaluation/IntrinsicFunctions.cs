5faa30214fcedf3c0ea48c186a18a530d79d9cd2
Rainer Sigwald <raines@microsoft.com>
2016-11-11 17:20:53 -0600

    Expose MSBuildToolsPath intrinsics for all bitness
    The previous implementation of MSBuildToolsPath32/64 was wrong when run
    from amd64 MSBuild: it would return the 64-bit path in the 32 property and
    an entirely wrong `bin/amd64/amd64` in the 64 property.
    
    Expose new intrinsics to get the correct values from all bitnesses.

--- src/XMakeBuildEngine/Evaluation/IntrinsicFunctions.cs
+++ src/XMakeBuildEngine/Evaluation/IntrinsicFunctions.cs
@@ -364,9 +364,19 @@ namespace Microsoft.Build.Evaluation
             return false;
         }
 
-        public static string GetCurrentExecutableDirectory()
+        public static string GetCurrentToolsDirectory()
         {
             return BuildEnvironmentHelper.Instance.CurrentMSBuildToolsDirectory;
+        }
+
+        public static string GetToolsDirectory32()
+        {
+            return BuildEnvironmentHelper.Instance.MSBuildToolsDirectory32;
+        }
+
+        public static string GetToolsDirectory64()
+        {
+            return BuildEnvironmentHelper.Instance.MSBuildToolsDirectory64;
         }
 
         public static string GetVsInstallRoot()
